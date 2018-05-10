edf14a699213e5ec6f6be5a070ea0ebaa0b513b5
artsykylee <knboomerang@gmail.com>
2018-05-10 11:34:52 -0400

    added click event functions and listners. added preloader for home page on initial load. and updated the links in the menu and logo to call page content from our WordPress API

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,9 @@
-// JavaScript Document
-
+// inside out project STEP-105
 window.onload = init();
 
 function init() {
 
-    window.addEventListener('scroll', function (e) {
+    window.addEventListener('scroll', function(e) {
         var distanceY = window.pageYOffset || document.documentElement.scrollTop,
             shrinkOn = 300,
             header = document.querySelector("header");
@@ -22,19 +21,18 @@ function init() {
         method: 'GET',
         url: 'https://me.creativeleekylee.com/wp-json/wp-api-menus/v2/menus/3',
         dataType: 'json',
-        success: function (data) {
+        success: function(data) {
             $('nav').hide();
             var menu = menuBuilder(data.items);
-
             $('nav').html(menu).slideDown();
-            
+            $('nav li a').click(function() {
+                getPage($(this).data("pgid"));
+            });
+            getPage(97);
             $("#loaderDiv").fadeOut("slow");
-
         },
-        error: function () {
-
+        error: function() {
             console.log('all is not good');
-
         }
     });
 
@@ -48,11 +46,8 @@ function menuBuilder(obj) {
     if (obj.length > 0) {
 
         theMenu = theMenu + '<ul>';
-
-        obj.forEach(function (item) {
-
-            theMenu = theMenu + '<li><a href="#">' + item.title + '</a>';
-
+        obj.forEach(function(item) {
+            theMenu = theMenu + '<li><a href="#" data-pgid="' + item.object_id + '">' + item.title + '</a>';
             if (item.children) {
 
                 theMenu = theMenu + menuBuilder(item.children);
@@ -60,7 +55,6 @@ function menuBuilder(obj) {
             }
 
             theMenu = theMenu + '</li>';
-
         });
 
         theMenu = theMenu + '</ul>';
@@ -72,4 +66,30 @@ function menuBuilder(obj) {
     }
 
     return theMenu;
+}
+
+function getPage(obj) {
+    $("#loaderDiv").fadeIn("slow");
+    $.ajax({
+        method: 'GET',
+        url: 'https://me.creativeleekylee.com/wp-json/wp/v2/pages/' + obj,
+        dataType: 'json',
+        success: function(data) {
+            var pgbuild = '';
+            pgbuild = '<section><div class="container">' + data.content.rendered + '</div></section>';
+            $("#content").fadeOut(function() {
+                $('html').animate({
+                    scrollTop: 0
+                }, 'slow'); //IE, FF
+                $('body').animate({
+                    scrollTop: 0
+                }, 'slow'); //chrome, don't know if Safari works
+                $(this).html(pgbuild).fadeIn();
+                $("#loaderDiv").fadeOut("slow");
+            });
+        },
+        error: function() {
+            console.log('bad');
+        }
+    });
 }
