0613cf9db9e25222e33a4808745c462a27753ab5
artsykylee <knboomerang@gmail.com>
2018-05-10 21:26:27 -0400

    changed my genLinks code in app.js to match what I learned in step 106-0

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,7 +3,7 @@ window.onload = init();
 
 function init() {
 
-    window.addEventListener('scroll', function(e) {
+    window.addEventListener('scroll', function (e) {
         var distanceY = window.pageYOffset || document.documentElement.scrollTop,
             shrinkOn = 300,
             header = document.querySelector("header");
@@ -21,65 +21,61 @@ function init() {
         method: 'GET',
         url: 'https://me.creativeleekylee.com/wp-json/wp-api-menus/v2/menus/3',
         dataType: 'json',
-        success: function(data) {
+        success: function (data) {
             $('nav').hide();
             var menu = menuBuilder(data.items);
             $('nav').html(menu).slideDown();
-            $('nav li a').click(function() {
+            $('nav li a').click(function () {
                 getPage($(this).data("pgid"));
             });
             getPage(97);
             $("#loaderDiv").fadeOut("slow");
         },
-        error: function() {
+        error: function () {
             console.log('all is not good');
         }
     });
+
 
     $.ajax({
         method: 'GET',
         url: 'https://me.creativeleekylee.com/wp-json/wp-api-menus/v2/menus/5',
         dataType: 'json',
-        success: function(data) {
+        success: function (data) {
             var menu = menuBuilder(data.items, 'genLinks', 'footer-ul');
+            console.log(data);
             $('#genLinks').replaceWith(menu);
-            $('#genLinks li a').click(function() {
+            $('#genLinks li a').click(function () {
                 getPage($(this).data("pgid"));
             });
         },
-        error: function() {
+        error: function () {
             console.log('all is not good');
         }
     });
 
 }
 
-function menuBuilder(obj) {
-
+function menuBuilder(obj, targetEl, classInfo) {
     var theMenu = '';
-
     if (obj.length > 0) {
-
-        theMenu = theMenu + '<ul>';
-        obj.forEach(function(item) {
+        let target = (targetEl) ? ' id="' + targetEl + '"' : '';
+        let elClass = (classInfo) ? ' class="' + classInfo + '"' : '';
+        theMenu = theMenu + '<ul' + target + '' + elClass + '>';
+        console.log(theMenu + ' ' + target);
+        obj.forEach(function (item) {
             theMenu = theMenu + '<li><a href="#" data-pgid="' + item.object_id + '">' + item.title + '</a>';
             if (item.children) {
-
                 theMenu = theMenu + menuBuilder(item.children);
 
             }
-
             theMenu = theMenu + '</li>';
         });
-
         theMenu = theMenu + '</ul>';
 
     } else {
-
         console.log('no data');
-
     }
-
     return theMenu;
 }
 
@@ -89,10 +85,10 @@ function getPage(obj) {
         method: 'GET',
         url: 'https://me.creativeleekylee.com/wp-json/wp/v2/pages/' + obj,
         dataType: 'json',
-        success: function(data) {
+        success: function (data) {
             var pgbuild = '';
             pgbuild = '<section><div class="container">' + data.content.rendered + '</div></section>';
-            $("#content").fadeOut(function() {
+            $("#content").fadeOut(function () {
                 $('html').animate({
                     scrollTop: 0
                 }, 'slow'); //IE, FF
@@ -103,33 +99,30 @@ function getPage(obj) {
                 $("#loaderDiv").fadeOut("slow");
             });
         },
-        error: function() {
+        error: function () {
             console.log('bad');
         }
     });
     getPosts();
 }
 
-function getPosts() {    
+function getPosts() {
+
     $.ajax({
         method: 'GET',
-        url: 'https://me.creativeleekylee.com/wp-json/wp/v2/posts?orderby=data&order=asc&per_page=5',
-        dataType: 'json',
-        success: function(data) {
+        url: 'https://me.creativeleekylee.com/wp-json/wp/v2/posts?orderby=date&order=asc&per_page5',
+        datatype: 'json',
+        success: function (data) {
             $("#latestPosts").html('<p id="postLdr"><i class="fa fa-cogs"></i> Loading Posts</p>');
-            data.forEach(function (item){
+            data.forEach(function (item) {
                 var myDate = new Date(item.date);
-                
-                $("latestPosts").prepend('<p>'+ item.title.rendered + '<span>' + myDate.getMonth()+ '-' + myDate.getDay()+ myDate.getFullYear() +'</span></p>');
-                
+                $("#latestPosts").prepend('<p>' + item.title.rendered + '<span>' + myDate.getMonth() + '-' + myDate.getDay() + '-' + myDate.getFullYear() + '</span></p>');
             });
             $("#postLdr").remove();
         },
-        error:function (){
+        error: function () {
             console.log('bad');
         }
+
     });
 }
-    
-
-                    
